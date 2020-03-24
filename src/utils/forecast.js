38871a11322e452a39e04468129ec34874a4857c
seantronsen <60711680+seantronsen@users.noreply.github.com>
2020-03-23 22:09:24 -0400

    Weather high for the day added.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast = ( longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + (body.currently.precipProbability*100) + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + (body.currently.precipProbability*100) + '% chance of rain. \nThe high for the day is '  + body.daily.data[0].temperatureMax + ' degrees.')
         }
     })
 }
